a7cde5c4babc1902861b1e87db17eaa2d976e663
Heidy saavedra <heidysaa0127@gmail.com>
2023-03-23 22:04:07 -0500

    mi primer commit

---
+++ data.js
@@ -0,0 +1,16 @@
+const data=[
+    {
+        Imag: "./ramdon.jpg",
+        frace:"Como agradezco la fortuna que tienen algunas personas de ser, musica,poesia y belleza con solo existir"
+    },
+    {
+        Imag:"./leer1.jpg",
+        frace:"Tomate un Momento para apreciar lo increibel que eres"
+    },
+    {
+        imag:"./tranqui1.jpg",
+        frace:"En una caja antigua de color rapé, un montón de semillas de naranjo, de pino, de cedro, de araucaria, de bellísima, de caobo y de amarillo. Esas semillas son pacientes y esperan su lugar y su tiempo"
+    },
+
+     
+]
